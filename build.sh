74efcd5652fb25cab149faf8b7f83c7fa885ffeb
Queryzi <system@nextflow.cloud>
2022-02-13 19:29:28 +0000

    Fix: Fix the build script

--- build.sh
+++ build.sh
@@ -1,3 +1,10 @@
-#/bin/bash
-
+#!/bin/bash
+existing_container_id=sudo docker ps -a -q --filter="name=sso-system_server_1"
+if [ ${#existing_container_id} == 0 ]
+then
+    echo ''
+else
+  sudo docker stop $existing_container_id
+  sudo docker rm $existing_container_id
+fi
 sudo docker-compose up -d --build --remove-orphans --force-recreate
