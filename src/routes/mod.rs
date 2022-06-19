860d339c6d229b5eb13e2e56b9bbf99d17b4be23
infiniwave <felixli101@outlook.com>
2022-06-19 11:17:29 -0500

    fix: restructure

--- src/routes/mod.rs
+++ src/routes/mod.rs
@@ -3,12 +3,10 @@ use warp::{filters::BoxedFilter, hyper::StatusCode, Filter, Reply};
 pub mod login;
 
 pub fn routes() -> BoxedFilter<(impl Reply,)> {
-    login::route()
-        .or(warp::path::end().map(|| {
-            warp::reply::with_status(
-                "I'm a teapot - never gonna give you up",
-                StatusCode::IM_A_TEAPOT,
-            )
-        }))
-        .boxed()
+    warp::path::end().map(|| {
+        warp::reply::with_status(
+            "I'm a teapot - never gonna give you up",
+            StatusCode::IM_A_TEAPOT,
+        )
+    }).or(login::route()).boxed()
 }
