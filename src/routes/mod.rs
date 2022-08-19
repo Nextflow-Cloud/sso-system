8f1126903653ae8fd7aff1ab696b65bdf65e61f2
infiniwave <felixli101@outlook.com>
2022-08-19 11:38:26 -0500

    fix: routes

--- src/routes/mod.rs
+++ src/routes/mod.rs
@@ -1,12 +1,20 @@
 use warp::{filters::BoxedFilter, hyper::StatusCode, Filter, Reply};
 
 pub mod login;
+pub mod register;
+pub mod validate;
 
 pub fn routes() -> BoxedFilter<(impl Reply,)> {
-    warp::path::end().map(|| {
+    login::route()
+    .or(register::route())
+    .or(validate::route())
+    .or(warp::path("api").map(|| {
         warp::reply::with_status(
             "I'm a teapot - never gonna give you up",
             StatusCode::IM_A_TEAPOT,
         )
-    }).or(login::route()).boxed()
+    }))
+    .or(warp::fs::dir("./bundle"))
+    .or(warp::fs::file("./bundle/index.html"))
+    .boxed()
 }
