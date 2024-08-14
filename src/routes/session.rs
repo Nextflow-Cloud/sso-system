9f0368ca6ffad25627ed0b67c60b1be67c601594
Infiniwave <61062461+infiniwave@users.noreply.github.com>
2024-08-14 01:51:38 +0000

    fix: sent token to client

--- src/routes/session.rs
+++ src/routes/session.rs
@@ -1,10 +1,18 @@
 use actix_web::{web, Responder};
 use futures_util::StreamExt;
 use mongodb::bson::doc;
+use serde::{Deserialize, Serialize};
 
 use crate::authenticate::Authenticate;
 use crate::database::session::{self, Session};
 use crate::errors::Result;
+
+#[derive(Deserialize, Serialize)]
+#[serde(rename_all = "camelCase")]
+pub struct ClientSession {
+    id: String,
+    friendly_name: String,
+}
 
 pub async fn handle(
     user_id: web::Path<String>,
@@ -21,6 +29,14 @@ pub async fn handle(
         .await
         .into_iter()
         .collect::<std::result::Result<Vec<Session>, _>>()?;
+    
+    let result = result
+        .into_iter()
+        .map(|session| ClientSession {
+            id: session.id,
+            friendly_name: session.friendly_name,
+        })
+        .collect::<Vec<ClientSession>>();
 
     Ok(web::Json(result))
 }
